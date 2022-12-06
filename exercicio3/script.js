fd2dd638f1d070b122d814fb4bb6c4fc356d4a9d
claraferreira <ferreirabatistaclara@gmail.com>
2022-12-06 12:12:40 -0300

    alt

---
+++ exercicio3/script.js
@@ -0,0 +1,23 @@
+// # Exercício 3
+
+// Crie um array com 5 strings. Faça um programa que percorra este array e imprima as strings em formato de ranking, como no exemplo abaixo:
+
+
+// **Utilize o for...of para resolver**
+// ```jsx
+// //entrada
+// const maioresPaises = ["Rússia", "Canadá", "China", "EUA", "Brasil"];
+
+// //saída
+// "1 - Rússia"
+// "2 - Canadá"
+// "3 - China"
+// "4 - EUA"
+// "5 - Brasil"
+// ```
+
+const maioresPaises = ["Rússia", "Canadá", "China", "EUA", "Brasil"]
+
+for (const i in maioresPaises) {
+    console.log(`${Number(i) + 1}  -  ${maioresPaises[i]}`)
+}
